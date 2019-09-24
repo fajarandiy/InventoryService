ecdec2164e94da58f26dfbe42120f593847e4673
user <user@AGUSTINUS-NB>
2019-09-24 14:20:22 +0700

    commit repository InventoryRepository

--- src/main/java/com/example/demo/inventory/repositories/InventoryItemRepository.java
+++ src/main/java/com/example/demo/inventory/repositories/InventoryItemRepository.java
@@ -2,8 +2,8 @@ package com.example.demo.inventory.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.demo.inventory.models.Inventory;
+import com.example.demo.inventory.models.InventoryItem;
 
-public interface InventoryItemRepository extends CrudRepository<Inventory, Integer>{
+public interface InventoryItemRepository extends CrudRepository<InventoryItem, Integer>{
 
 }
