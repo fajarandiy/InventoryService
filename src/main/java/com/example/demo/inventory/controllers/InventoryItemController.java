72a23869f4c299a5d4ceb48fdfda49b935aa8f18
user <user@AGUSTINUS-NB>
2019-09-24 14:08:25 +0700

    InventoryItem

--- src/main/java/com/example/demo/inventory/controllers/InventoryItemController.java
+++ src/main/java/com/example/demo/inventory/controllers/InventoryItemController.java
@@ -1,5 +1,32 @@
 package com.example.demo.inventory.controllers;
 
-public class InventoryItemController {
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.inventory.models.InventoryItem;
+import com.example.demo.inventory.repositories.InventoryItemRepository;
+
+@RestController
+@RequestMapping("api/inventoryItem")
+public class InventoryItemController {
+	@Autowired
+	private InventoryItemRepository repo;
+	
+	@PostMapping("/create")
+	public String createInventoryItem(@Valid @RequestBody InventoryItem obj) {
+		repo.save(obj);
+		
+		return "success create";
+	}
+	
+	@GetMapping("/getall")
+	public Iterable<InventoryItem> getAll() {
+		return repo.findAll();
+	}
 }
